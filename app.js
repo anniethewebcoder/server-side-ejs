db70201ab5cff80be8e372fa40472057de881ad4
anniethewebcoder <anniethewebcoder@gmail.com>
2024-05-21 08:48:08 -0400

    flash

--- app.js
+++ app.js
@@ -4,16 +4,63 @@ require("dotenv").config()
 
 const session = require("express-session")
 
+const MongoDBStore = require("connect-mongodb-session")(session)
+const url = process.env.MONGO_URI
+
 const app = express()
 
-app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true
-    })
-)
+// app.use(
+//     session({
+//         secret: process.env.SESSION_SECRET,
+//         resave: false,
+//         saveUninitialized: true
+//     })
+// )
 
+// SESSIONS
+const store = new MongoDBStore({
+    uri: url,
+    collection: "mySessions"
+})
+
+store.on("error", function(error) {
+    console.log(error)
+})
+
+const sessionParams = {
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+        secure: false,
+        sameSite: "strict"
+    }
+}
+
+if(app.get("env") === "production") {
+    app.set("trust proxy", 1)
+    sessionParams.cookie.secure = true
+}
+
+app.use(session(sessionParams))
+
+// FLASH MESSAGES
+app.use(require("connect-flash")())
+
+app.post("/secretWord", (req, res) => {
+    if(req.body.secretWord.toUppercase()[0] == "P") {
+        req.flash("error", "That word won't work!")
+        req.flash("error", "You can't use words that start with p.")
+    } else {
+        req.session.secretWord = req.body.secretWord
+        req.flash("info", "The secret word was changed.")
+    }
+
+    res.redirect("/secretWord")
+})
+
+// SECRET WORD VIEW
 app.set("view engine", "ejs")
 app.use(require("body-parser").urlencoded({ extended: true }))
 
@@ -21,6 +68,9 @@ app.get("/secretWord", (req, res) => {
     if(!req.session.secretWord) {
         req.session.secretWord = "syzygy"
     }
+
+    res.locals.info = req.flash("info");
+    res.locals.errors = req.flash("error");
 
     res.render("secretWord", {
         secretWord: req.session.secretWord
@@ -40,6 +90,7 @@ app.use((err, req, res, next) => {
     res.status(500).send(err.message)
     console.log(err)
 })
+
 
 const port = process.env.PORT || 3000
 
