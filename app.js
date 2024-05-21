900aa5f77779edf544395c59b84a31d018a5fa57
anniethewebcoder <anniethewebcoder@gmail.com>
2024-05-21 17:00:47 -0400

    fixing the post

--- app.js
+++ app.js
@@ -1,108 +1,84 @@
-const express = require("express")
-require("express-async-errors")
-require("dotenv").config()
+const express = require("express");
+require("express-async-errors");
 
-const session = require("express-session")
+const app = express();
 
-const MongoDBStore = require("connect-mongodb-session")(session)
-const url = process.env.MONGO_URI
+require("dotenv").config(); // to load the .env file into the process.env object
+const session = require("express-session");
 
-const app = express()
+const MongoDBStore = require("connect-mongodb-session")(session);
+const url = process.env.MONGO_URI;
 
-// app.use(
-//     session({
-//         secret: process.env.SESSION_SECRET,
-//         resave: false,
-//         saveUninitialized: true
-//     })
-// )
-
-// SESSIONS
 const store = new MongoDBStore({
-    uri: url,
-    collection: "mySessions"
-})
+  // may throw an error, which won't be caught
+  uri: url,
+  collection: "mySessions",
+});
+store.on("error", function (error) {
+  console.log(error);
+});
 
-store.on("error", function(error) {
-    console.log(error)
-})
+const sessionParms = {
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: store,
+  cookie: { secure: false, sameSite: "strict" },
+};
 
-const sessionParams = {
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    store: store,
-    cookie: {
-        secure: false,
-        sameSite: "strict"
-    }
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sessionParms.cookie.secure = true; // serve secure cookies
 }
 
-if(app.get("env") === "production") {
-    app.set("trust proxy", 1)
-    sessionParams.cookie.secure = true
-}
+app.use(session(sessionParms));
 
-app.use(session(sessionParams))
+app.use(require("connect-flash")());
 
-// FLASH MESSAGES
-app.use(require("connect-flash")())
+app.set("view engine", "ejs");
+app.use(require("body-parser").urlencoded({ extended: true }));
 
-app.post("/secretWord", (req, res) => {
-    if(req.body.secretWord.toUppercase()[0] == "P") {
-        req.flash("error", "That word won't work!")
-        req.flash("error", "You can't use words that start with p.")
-    } else {
-        req.session.secretWord = req.body.secretWord
-        req.flash("info", "The secret word was changed.")
-    }
-
-    res.redirect("/secretWord")
-})
-
-// SECRET WORD VIEW
-app.set("view engine", "ejs")
-app.use(require("body-parser").urlencoded({ extended: true }))
-
+// secret word handling
 app.get("/secretWord", (req, res) => {
-    if(!req.session.secretWord) {
-        req.session.secretWord = "syzygy"
+    if (!req.session.secretWord) {
+      req.session.secretWord = "syzygy";
     }
-
     res.locals.info = req.flash("info");
     res.locals.errors = req.flash("error");
-
-    res.render("secretWord", {
-        secretWord: req.session.secretWord
-    })
-})
+    res.render("secretWord", { secretWord: req.session.secretWord });
+});
 
 app.post("/secretWord", (req, res) => {
-    secretWord = req.body.secretWord
-    res.redirect("/secretWord")
-})
+    if (req.body.secretWord.toUpperCase()[0] == "P") {
+      req.flash("error", "That word won't work!");
+      req.flash("error", "You can't use words that start with p.");
+    } else {
+      req.session.secretWord = req.body.secretWord;
+      req.flash("info", "The secret word was changed.");
+    }
+    res.redirect("/secretWord");
+});
+  
 
 app.use((req, res) => {
-    res.status(404).send(`That page (${req.url}) was not found.`)
-})
+  res.status(404).send(`That page (${req.url}) was not found.`);
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send(err.message)
-    console.log(err)
-})
+  res.status(500).send(err.message);
+  console.log(err);
+});
 
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const start = async() => {
-
-    try {
-        app.listen(port, () => {
-            console.log(`Server is listening on port ${port}...`)
-        })
-    } catch(err) {
-        console.log(err)
-    }
-}
-
-start()
+start();
